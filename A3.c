1f068db35d351fd0b37d3c85c4340b2615eb5133
ranjumol <46959511+ranjumol@users.noreply.github.com>
2019-02-16 02:20:08 -0800

    Create A3.c

---
+++ A3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void) 
+{
+int n,remainder,reverse=0;
+int result;
+scanf("%d",&n);
+while(n!=0)
+{
+remainder=n%10;
+reverse=reverse*10+remainder;
+n=n/10;
+}
+printf("\n%d",reverse);
+
+return 0;
+
+ 
+
+}
